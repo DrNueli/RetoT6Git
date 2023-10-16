d3a604e1c56cc44fb1f6c6f8f223717c2ebb6081
DrNueli <manuelmorenoacn@gmail.com>
2023-10-16 23:44:58 +0200

    Añadir fc cuadrado y repo

---
+++ index.js
@@ -0,0 +1,15 @@
+// Tema 5. Funciones
+
+// RETO 2:  • En el fichero index.js (fichero de librería):
+// - Declarar las funciones de la calculadora por separado (suma, resta, producto, división)
+// - Al final del documento index.js escribir la sentencia:  module.exports =
+// A continuación escribir el nombre de las funciones entre {} separados por comas de la librería que se desean exportar.
+
+let cuadrado = (number) => { let resultado = number * number; return resultado};
+let subs = (op1, op2) => { let resultado = op1 - op2; return resultado};
+let mult = (op1, op2) => { let resultado = op1 * op2; return resultado};
+let div = (op1, op2) => { let resultado = op1 / op2; return resultado};
+
+// console.log(mult(2,5));
+
+module.exports = {cuadrado, subs, mult, div};
---
+++ run.js
@@ -0,0 +1,7 @@
+// Tema 5: Reto 2
+
+let myLib = require("./index");
+
+console.log(myLib.sum(2,3)); // veo que es necesario hacer un console.log para imprimir el resultado, eso es correcto??
+
+//Reto 3: Se define un vector como un array de números. Crea un fichero llamado libreriaVector.js con las siguientes funciones:
