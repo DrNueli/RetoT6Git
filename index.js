281f611e9306ed1f9bb0fa916dabf27274d08862
DrNueli <manuelmorenoacn@gmail.com>
2023-10-17 20:27:21 +0200

    borrado fx suma

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 // - Al final del documento index.js escribir la sentencia:  module.exports =
 // A continuación escribir el nombre de las funciones entre {} separados por comas de la librería que se desean exportar.
 
-let cuadrado = (number) => { let resultado = number * number; return resultado};
 let subs = (op1, op2) => { let resultado = op1 - op2; return resultado};
 let mult = (op1, op2) => { let resultado = op1 * op2; return resultado};
 let div = (op1, op2) => { let resultado = op1 / op2; return resultado};
