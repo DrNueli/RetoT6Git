c519f3c7eed8b3cfafbabc35991db5fb4d2ebf42
DrNueli <manuelmorenoacn@gmail.com>
2023-10-17 20:31:24 +0200

    suma 3 paramet

--- index.js
+++ index.js
@@ -5,11 +5,11 @@
 // - Al final del documento index.js escribir la sentencia:  module.exports =
 // A continuación escribir el nombre de las funciones entre {} separados por comas de la librería que se desean exportar.
 
-let cuadrado = (number) => { let resultado = number * number; return resultado};
+let suma = (number) => { let resultado = number + number + number; return resultado};
 let subs = (op1, op2) => { let resultado = op1 - op2; return resultado};
 let mult = (op1, op2) => { let resultado = op1 * op2; return resultado};
 let div = (op1, op2) => { let resultado = op1 / op2; return resultado};
 
 // console.log(mult(2,5));
 
-module.exports = {cuadrado, subs, mult, div};
+module.exports = {suma, subs, mult, div};
